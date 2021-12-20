ef7a3f235a7f63e38404c88aa2019eb57776a70c
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2021-12-20 16:58:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,9 +20,6 @@ async def start(client, message):
             [
                 InlineKeyboardButton('🤖 Updates', url='https://t.me/MovieClubUpdates')
             ],
-            [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -36,13 +33,9 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+        buttons = [
             InlineKeyboardButton('🤖 Updates', url='https://t.me/MovieClubUpdates')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
-        ]]
+            ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -75,13 +68,9 @@ async def start(client, message):
             )
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+        buttons = [
             InlineKeyboardButton('🤖 Updates', url='https://t.me/MovieClubUpdates')
-            ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
-        ]]
+            ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
