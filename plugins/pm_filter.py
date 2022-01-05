ebff718868f0e15a30b5c7799f3aa5db78063169
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-01-05 20:08:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -639,7 +639,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"මේ තියෙන්නෙ ඔයා හොයන {search} 👇"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
