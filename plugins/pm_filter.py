7cd40448980def0379a45c2a1ff1bbaf3626c72e
nrp155 <84077310+nrp155@users.noreply.github.com>
2023-10-23 11:29:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("This is someone else's search result,(මේ වෙන කෙනෙක්ගේ සර්ච් රිසල්ට් එකක්)", show_alert=True)
+        return await query.answer("This is someone else's search result,මේ වෙන කෙනෙක්ගේ සර්ච් රිසල්ට් එකක්", show_alert=True)
     try:
         offset = int(offset)
     except:
